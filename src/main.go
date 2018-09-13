4b0b5a7f875e99040305ea31361ac27e54a1c2fa
ryota suzuki <ryota@fringe81.com>
2018-09-13 10:41:14 +0900

    WIP: structをloggerで出して見る

--- src/main.go
+++ src/main.go
@@ -8,4 +8,28 @@ func main() {
 	sugar := logger.Sugar()
 	url := "http://localhost"
 	sugar.Infof("Failed to fetch URL: %s", url)
+
+	logic(*logger)
+}
+
+type Record struct {
+	Name string
+}
+
+func newRecord(name string) Record {
+	record := Record{}
+	record.Name = name
+	return record
+}
+
+func logic(logger zap.Logger) {
+	logger.Info("startLogic")
+
+	array := "HOUGE" + "HUGE"
+
+	logger.Info(array)
+	record := newRecord("new!")
+	logger.Info(record)
+
+	logger.Info("endLogic")
 }
